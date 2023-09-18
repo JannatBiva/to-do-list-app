9859101a0c4e8ecd9de8b4a3ebe2c675dc6fe57b
Jannatul Tazrin <jannatultazrin01@gmail.com>
2023-09-18 23:23:55 +0300

    edited done, edit button and added bg

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js (or script.js) - Client-side JavaScript
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('todo-form');
   const taskInput = document.getElementById('task');
@@ -8,12 +7,48 @@ document.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     const taskText = taskInput.value.trim();
     if (taskText) {
+      // Create a task item
       const taskItem = document.createElement('li');
-      taskItem.textContent = taskText;
-      taskList.appendChild(taskItem);
-      taskInput.value = '';
 
-      // Add logic here to send the task to the server (e.g., via an API).
+      // Task text
+      const taskTextElement = document.createElement('span');
+      taskTextElement.textContent = taskText;
+
+      // Edit button
+      const editButton = document.createElement('button');
+      editButton.textContent = 'Edit';
+      editButton.addEventListener('click', () => {
+        const updatedText = prompt('Edit task:', taskTextElement.textContent);
+        if (updatedText !== null) {
+          taskTextElement.textContent = updatedText;
+          // You can add logic here to send the updated task to the server (e.g., via an API).
+        }
+      });
+
+      // Done button
+      const doneButton = document.createElement('button');
+      doneButton.textContent = 'Done';
+      doneButton.addEventListener('click', () => {
+        // Apply a strike-through effect
+        taskTextElement.style.textDecoration = 'line-through';
+        
+        // Remove the task item after a brief delay (you can adjust the delay as needed)
+        setTimeout(() => {
+          taskItem.remove();
+        }, 1000); // Remove after 1 second (1000 milliseconds)
+      });
+      
+      taskTextElement.style.marginRight = '10px';
+
+      // Append elements to the task item
+      taskItem.appendChild(taskTextElement);
+      taskItem.appendChild(editButton);
+      taskItem.appendChild(doneButton);
+
+      // Add the task item to the task list
+      taskList.appendChild(taskItem);
+
+      taskInput.value = '';
     }
   });
 });
